988aa6fa12ea87bca881da2e1de8dbfa704dd484
Matheus Pansani Marega <142553250+Matheus-Marega@users.noreply.github.com>
2024-10-15 18:17:54 -0300

    Update pages.py

--- user_PI/app/pages.py
+++ user_PI/app/pages.py
@@ -1,7 +1,12 @@
 import streamlit as st
 from pydantic import ValidationError
-from dataContract import User, lojas_shopping, lista_segmentos, nota
+from dataContract import User,lista_segmentos,escala_satisfacao,frequencia_visita,sim_nao,genero
+from DataBase import PerguntasInformacoesClientes, PerguntasEstruturaMall, PerguntasLojasVisitadas, engine
+from sqlalchemy.orm import sessionmaker
 
+# Criação da sessão para interagir com o banco de dados
+Session = sessionmaker(bind=engine)
+session = Session()
 
 def pagina_cadastro():
     st.title("Formulário de Cadastro")
@@ -16,25 +21,22 @@ def pagina_cadastro():
     if 'gender' not in st.session_state:
         st.session_state.gender = 'Masculino'  
 
-    genero = ["Masculino", "Feminino", "Outros"]
-
     # Criação dos campos com valores guardados no session_state
     st.session_state.nome = st.text_input("Nome", value=st.session_state.nome)
     st.session_state.email = st.text_input("Email", value=st.session_state.email)
     st.session_state.data = st.date_input("Data da Visita", value=st.session_state.data)
     st.session_state.gender = st.selectbox("Gênero", genero, index=genero.index(st.session_state.gender))
 
-
+    #Botão de proxima pagina, que quando clicado, valida as Informações preenchidas
     if st.button("Próximo >>"):
         try:
             dados_usuario = User(
                 nome=st.session_state.nome, 
-                email=st.session_state.email,
+                email=st.session_state.email,       #Validação das infomações
                 data=st.session_state.data,
                 gender=st.session_state.gender
             )
             st.success("Dados cadastrados com sucesso!")
-            st.write(dados_usuario)
             st.session_state.pagina = 'pagina2'
         except ValidationError as e:
             st.error(f"Erro ao cadastrar os dados: {e}")
@@ -42,25 +44,23 @@ def pagina_cadastro():
 def perguntas_estrutura():
     st.title("Perguntas Sobre a estrutura do Mall")
 
-
-    # De acordo com as Perguntas, alterar as funções, a estrutura abaixo, esta validando apenas dados em STR.
+    ## O codigo abaixo verifica se as variaveis possuem valor zerado para que se recarregado a pagina, as informações sejam salvas com os Dados do Usuario
     if 'p1_pe' not in st.session_state:
-        st.session_state.p1_pe = ''
+        st.session_state.p1_pe = '-'
     if 'p2_pe' not in st.session_state:
-        st.session_state.p2_pe = ''
+        st.session_state.p2_pe = '-'
     if 'p3_pe' not in st.session_state:
-        st.session_state.p3_pe = ''
+        st.session_state.p3_pe = '-'
     if 'p4_pe' not in st.session_state:
-        st.session_state.p4_pe = ''
+        st.session_state.p4_pe = '-'
     if 'sm_pe' not in st.session_state:
         st.session_state.sm_pe = ''  
 
-    # De acordo com as Perguntas, alterar as funções, a estrutura abaixo, esta validando apenas dados em STR.
-    st.session_state.p1_pe = st.text_input("Pergunta 1", value=st.session_state.p1_pe)
-    st.session_state.p2_pe = st.text_input("Pergunta 2", value=st.session_state.p2_pe)
-    st.session_state.p3_pe = st.text_input("Pergunta 3", value=st.session_state.p3_pe)
-    st.session_state.p4_pe = st.text_input("Pergunta 4", value=st.session_state.p4_pe)
-    st.session_state.sm_pe = st.text_area("Sugestão de Melhoria", value=st.session_state.sm_pe)
+    st.session_state.p1_pe = st.selectbox(f"Como você avalia a infraestrutura geral do shopping {st.session_state.nome}?",escala_satisfacao, index=escala_satisfacao.index(st.session_state.p1_pe))
+    st.session_state.p2_pe = st.selectbox("Com que frequência você visita o shopping?",frequencia_visita, index=frequencia_visita.index(st.session_state.p2_pe))
+    st.session_state.p3_pe = st.selectbox("Como você avalia a limpeza e manutenção das áreas comuns do shopping?",escala_satisfacao, index=escala_satisfacao.index(st.session_state.p3_pe))
+    st.session_state.p4_pe = st.selectbox("Você se sente seguro(a) durante sua visita ao shopping?", sim_nao,index=sim_nao.index(st.session_state.p4_pe))
+    st.session_state.sm_pe = st.text_area("Quais melhorias você sugeriria para a experiência geral no shopping?", value=st.session_state.sm_pe)
 
     if st.button("Proximo >>"):
         st.session_state.pagina = 'pagina3'
@@ -75,24 +75,58 @@ def perguntas_lojas():
     if 'p1_pl' not in st.session_state:
         st.session_state.p1_pl = "Nenhuma"
     if 'p2_pl' not in st.session_state:
-        st.session_state.p2_pl = 'Nenhuma'
+        st.session_state.p2_pl = "-"
     if 'p3_pl' not in st.session_state:
-        st.session_state.p3_pl = '-'
+        st.session_state.p3_pl = "-"
     if 'p4_pl' not in st.session_state:
-        st.session_state.p4_pl = ''
+        st.session_state.p4_pl = "-"
+    if 'p5_pl' not in st.session_state:
+        st.session_state.p5_pl = "-"
     if 'sm_pl' not in st.session_state:
         st.session_state.sm_pl = ''  
 
         
-    st.session_state.p1_pl = st.selectbox("Segmento: ",lista_segmentos, index=lista_segmentos.index(st.session_state.p1_pl))
-    st.session_state.p2_pl = st.selectbox("Lojas Visitadas",lojas_shopping, index=lojas_shopping.index(st.session_state.p2_pl))
-    st.session_state.p3_pl = st.selectbox("Nota de atendimento e produto",nota, index=nota.index(st.session_state.p3_pl))
-    st.session_state.p4_pl = st.text_input("Pergunta Adicional", value=st.session_state.p4_pl)
-    st.session_state.sm_pl = st.text_area("Sugestão de Melhoria", value=st.session_state.sm_pl)
+    
+    st.session_state.p1_pl = st.selectbox("Dentre as Lojas visitadas, qual segmento você mais frequenta?: ",lista_segmentos, index=lista_segmentos.index(st.session_state.p1_pl))
+    st.session_state.p2_pl = st.selectbox("Você encontrou facilidade para localizar as lojas que estava procurando?",sim_nao, index=sim_nao.index(st.session_state.p2_pl))
+    st.session_state.p3_pl = st.selectbox("Como você avalia o atendimento nas lojas que visitou?",sim_nao, index=sim_nao.index(st.session_state.p3_pl))
+    st.session_state.p4_pl = st.selectbox("As lojas oferecem produtos que atendem suas necessidades e expectativas?",sim_nao, index=sim_nao.index(st.session_state.p4_pl))
+    st.session_state.p5_pl = st.selectbox("Como você avalia a variedade de lojas disponíveis no shopping?: ",escala_satisfacao, index=escala_satisfacao.index(st.session_state.p5_pl))
+    st.session_state.sm_pl = st.text_area("Sugestão Quais produtos ou serviços você gostaria de ver nas lojas que ainda não estão disponíveis? Melhoria", value=st.session_state.sm_pl)
 
 
     if st.button("Finalizar >>"):
         st.session_state.pagina = 'pagina4'
+
+        #Adicionando as Informações coletadas ao Banco de Dados
+        insercao_informacoes_cliente = PerguntasInformacoesClientes(
+            nome=st.session_state.nome, 
+            email=st.session_state.email,
+            data_visita=st.session_state.data,
+            genero=st.session_state.gender
+        )
+
+        insercao_respostas_perguntas_estrutura_mall = PerguntasEstruturaMall(
+            pergunta1_estrutura_mall = st.session_state.p1_pe,
+            pergunta2_estrutura_mall = st.session_state.p2_pe,
+            pergunta3_estrutura_mall = st.session_state.p3_pe,
+            pergunta4_estrutura_mall = st.session_state.p4_pe,
+            pergunta5_estrutura_mall = st.session_state.sm_pe
+        )
+
+        insercao_respostas_perguntas_lojas_visitadas = PerguntasEstruturaMall(
+            pergunta1_lojas_visitadas = st.session_state.p1_pl,
+            pergunta2_lojas_visitadas = st.session_state.p2_pl,
+            pergunta3_lojas_visitadas = st.session_state.p3_pl,
+            pergunta4_lojas_visitadas = st.session_state.p4_pl,
+            pergunta5_lojas_visitadas = st.session_state.sm_pl
+        )
+
+        session.add(insercao_informacoes_cliente)
+        session.add(insercao_respostas_perguntas_estrutura_mall)
+        session.add(insercao_respostas_perguntas_lojas_visitadas)
+        session.commit()
+
     if st.button("<< Voltar"):
         st.session_state.pagina = 'pagina2'
 
