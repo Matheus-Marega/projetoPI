6ced912f4f29b00dca924e80f8e18d138e5b1761
Matheus Pansani Marega <142553250+Matheus-Marega@users.noreply.github.com>
2024-10-17 20:18:43 -0300

    Add files via upload

---
+++ projetoPI-main/projeto_PI/codigo_paginas_formulario_e_DB/config/lists_for_functions.py
@@ -0,0 +1,53 @@
+lojas_shopping = [
+    "Nenhuma",
+    "Loja do Esporte",
+    "Moda & Estilo",
+    "Tecnologia Top",
+    "Café Gourmet",
+    "Livraria Central",
+    "Brinquedos & Cia",
+    "Joias de Luxo",
+    "Mundo dos Games",
+    "Cine Shopping",
+    "Super Beleza"
+]
+
+genero = ["Masculino", "Feminino", "Outros", "Prefiro não Responder"]
+
+escala_satisfacao = [
+    "-",
+    "1 - Insatisfeito",
+    "2 - Pouco Satisfeito",
+    "3 - Satisfeito",
+    "4 - Bem Satisfeito",
+    "5 - Muito Satisfeito"
+]
+
+frequencia_visita = [
+    "-",
+    "Diaramente",
+    "Semanalmente",
+    "Mensalmente",
+    "Raramente"
+]
+
+sim_nao = [
+    "-",
+    "Não",
+    "Parcialmente",
+    "Sim"
+]
+
+lista_segmentos = [
+    "Nenhuma",
+    "Roupas e Acessórios",
+    "Calçados",
+    "Eletrônicos e Tecnologia",
+    "Alimentação (Praça de Alimentação)",
+    "Cosméticos e Perfumaria",
+    "Jóias e Relógios",
+    "Livrarias e Papelarias",
+    "Brinquedos e Jogos",
+    "Esportes e Lazer",
+    "Decoração e Utilidades Domésticas"
+]
