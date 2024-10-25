ae1eaaa21bfc52072f9426dd1a223b1609e8c24e
Matheus Pansani Marega <142553250+Matheus-Marega@users.noreply.github.com>
2024-10-25 15:38:20 -0300

    Delete projeto_PI directory

--- projeto_PI/codigo_paginas_formulario_e_DB/forms/user_form.py
+++
@@ -1,8 +0,0 @@
-# Formulário de usuário com validações de dados
-
-from pydantic import BaseModel,EmailStr
-
-class User(BaseModel):
-    email: EmailStr
-    nome: str
-    gender:str
--- projeto_PI/codigo_paginas_formulario_e_DB/services/data_processing.py
+++
@@ -1,2 +0,0 @@
-# Processamento de dados para exibição no Streamlit (e.g., gráficos)
-
