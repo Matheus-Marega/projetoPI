71805fccdb0a6d27456b091d81877f651935c0e9
Matheus Pansani Marega <142553250+Matheus-Marega@users.noreply.github.com>
2024-10-25 17:24:06 -0300

    Add files via upload

---
+++ projeto_PI/codigo_paginas_formulario_e_DB/database/db_setup.py
@@ -0,0 +1,23 @@
+# Configuração e inicialização da conexão com o banco
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+DATABASE_URL = os.getenv("MYSQL_PUBLIC_URL")
+
+# Configurar o engine de conexão com o banco de dados
+engine = create_engine(DATABASE_URL)
+
+# Configurar a sessão para interagir com o banco de dados
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Função para obter a sessão
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
