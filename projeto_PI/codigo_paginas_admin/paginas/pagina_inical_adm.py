fc54bb520cf5b3f57dee3f4cdccddfb2d58f561e
Matheus Pansani Marega <142553250+Matheus-Marega@users.noreply.github.com>
2024-10-28 22:42:35 -0300

    Delete para ajuste de conexão com o DB

--- projeto_PI/codigo_paginas_admin/paginas/pagina_inical_adm.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-def mostrar_relatorio_semanal():
-    st.title("Teste de relatorio semanal")
--- projeto_PI/codigo_paginas_admin/paginas/pagina_sugetsao_melhorias.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def mostrar_sugestao_melhorias_feedback():
-    st.title("Teste de Feedback")
-
